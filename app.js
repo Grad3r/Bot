e3f1f11e766058c18dc063e53fe1cb4f355972f7
Dima <grad3r@ex.ua>
2019-02-04 08:09:28 +0200

    Artificial Intelligence

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const toki = require('./tokin').token;
 const bot = new TelegramBot(toki, {polling: true});
 
 const Sequelize = require('sequelize');
-
 const sequelize = new Sequelize('carsofmegacom', 'root', 'password', {
     host: 'localhost',
     dialect: 'mysql',
@@ -15,18 +14,27 @@ sequelize.query("SELECT * FROM `fiat`", {type: sequelize.QueryTypes.SELECT})
 // Якщо виконання успішне, то працює then
     .then(materials => {
         console.log(materials);
+try {
+    let materials1 = JSON.parse(materials, function (key, value) {
+        if (key === 'ONU') return new id(value);
+        return value
+    });
+}
+catch (e) {
+        materials1 = materials;
+}
+
+console.log(materials1.ONU)
+
+
+        bot.onText(/\Fiat/, function (msg, match) {
+            bot.sendMessage(msg.chat.id, 'В автомобілі FIAT у наявності є такі матеріали:' )
+        });
     })
-    // Якщо виконання не було успішним, то помила попада в catch
+    // Якщо виконання не було успішним, то помила попадає в catch
     .catch(err => {
         console.log(err);
     });
-
-
-bot.onText(/\Fiat/, function (msg, match) {
-
-    // let mat = f();
-    bot.sendMessage(msg.chat.id, 'В автомобілі FIAT у наявності є такі матеріали:', mat);
-});
 
 
 bot.onText(/\/start/, function (msg, match) {
@@ -43,15 +51,14 @@ const automobile = {
 
 
 bot.onText(/\Peugeot/, function (msg, match) {
-    bot.sendMessage(msg.chat.id, 'В автомобілі PEUGEOT у наявності є такі матеріали:', materials1);
+    bot.sendMessage(msg.chat.id, 'В автомобілі PEUGEOT у наявності є такі матеріали:', materials);
 });
-let materials1 = {
+let materials = {
     reply_markup: JSON.stringify({
         inline_keyboard: [
             [{text: 'ONU', callback_data: '1'}],
             [{text: 'OpticalBox', callback_data: 'data 2'}],
         ]
-
     })
 };
 
